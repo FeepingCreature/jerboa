31198024123f01e3f23f0de5eff588726482edd2
FeepingCreature <default_357-line@yahoo.de>
2016-09-09 09:09:55 +0200

    gen headers for libpng and C stdlib

--- genlibs.sh
+++ genlibs.sh
@@ -13,5 +13,9 @@ mkdir -p c
 ./swigify.sh -p glfwq -p GLFWQ_ -p GLFWQ -x gl -x GL -x PFNGL /usr/include/glfwq.h libglfwq.so c/glfwq.jb
 ./swigify.sh -p sf /usr/include/SFML/Audio.h libcsfml-audio.so c/sfml-audio.jb -cpperraswarn -I/usr/include/ -DsfInt64=int64_t
 ./swigify.sh -p sf /usr/include/SFML/System.h libcsfml-system.so c/sfml-system.jb -cpperraswarn -I/usr/include/ -DsfInt64=int64_t
+./swigify.sh -p png_ -p PNG_ /usr/include/png.h libpng.so c/png.jb -cpperraswarn
 ./swigify.sh -p SOIL_ /usr/include/SOIL.h libSOIL.so c/soil.jb
 ./swigify.sh /usr/include/time.h librt.so c/time.jb
+./swigify.sh -x __ /usr/include/stdio.h libc.so.6 c/stdio.jb
+./swigify.sh -x __ /usr/include/stdlib.h libc.so.6 c/stdlib.jb
+./swigify.sh -x __ /usr/include/string.h libc.so.6 c/string.jb
